7e30bd551ac9d76d2a435009345cbebeb703a17c
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2019-11-27 18:15:01 +0100

    updated to max error

--- gt/numerics/convergence_tests_mt.hpp
+++ gt/numerics/convergence_tests_mt.hpp
@@ -8,7 +8,6 @@
 #include "../verification/run.hpp"
 #include "solver.hpp"
 
-#include <atomic>
 #include <thread>
 
 template <class CommWorld> void run_convergence_tests(CommWorld &&comm_world, int num_threads = 1)
@@ -20,22 +19,24 @@ template <class CommWorld> void run_convergence_tests(CommWorld &&comm_world, in
         {
             auto comm_grid = communication::grid_mt(comm_world, {resolution, resolution, resolution});
             using grid_t = communication::grid_mt::sub_grid_type;
-            std::atomic<double> ret(0.0);
             std::vector<std::thread> threads;
             threads.reserve(num_threads);
+            std::vector<double> errors(num_threads, 0.0);
 
             for (int i=0; i<num_threads; ++i)
             {
                 threads.push_back( std::thread{
-                    [&ret,exact](communication::grid_mt& grid, int thread_id)
+                    [&ret,exact,errors](communication::grid_mt& grid, int thread_id)
                     {
-                        ret += run(grid[thread_id], hdiff_stepper(exact.diffusion_coeff), 1e-4, 1e-5, exact);
+                        errors[thread_id] = run(grid[thread_id], hdiff_stepper(exact.diffusion_coeff), 1e-4, 1e-5, exact);
                     },
                     std::ref(comm_grid),
                     i});
             }
-            const auto error = ret.load();
-            return std::sqrt(communication::global_sum(comm_grid, error));
+            auto error = errors[0];
+            for (unsigned int i=1; i<errors.size(); ++i)
+                error = std::max(errors[i], error);
+            return std::sqrt(communication::global_max(comm_grid, error));
         };
         print_order_verification_result(order_verification(error_f, 8, 128));
     }
